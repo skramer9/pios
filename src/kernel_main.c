925de641f439a62c65db016162587902a003e968
Sam Kramer <skramer5@luc.edu>
2024-09-11 19:28:57 -0500

    made clear_bss method, seperate from main

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -11,18 +11,21 @@ wait() { //waits 1 ms
     int time;
 }
 
-
-void kernel_main() {
-
+void clear_bss() { // set all of bss to 0
     extern int __bss_start, __bss_end; //external variables, from kernel.ld
     char *bssstart, *bssend;
 
     bssstart = &__bss_start;
     bssend = &__bss_end;
 
-    for (int i = 0; i <= bssend - bssstart; i++) { //set bss to clear
+    for (int i = 0; i <= bssend - bssstart; i++) {
         bssstart[i] = 0;
-    }
+    }    
+}
+
+void kernel_main() {
+
+    clear_bss();
 
     int time = get_timer_count();
 
