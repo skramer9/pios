5e232ef935eda947caea14930d714f6c47eb92d5
Sam Kramer <skramer5@luc.edu>
2024-09-16 14:23:59 -0500

    Gave up on making add work the way i thought it would. Committed this in case i want to come back to it.

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -9,24 +9,34 @@ struct list_element { //element for a linked list
 
 //adds an element to a linked list
 //@param *list_head = a list_element which points to the first element in the list
-//@param *new_element = the new element to be added before its next value
+//@param *new_element = the new element to be added before its next value (PASS IN THE & OF THE ELEMENT)
+
 list_add(struct list_element *list_head, struct list_element *new_element) {
-    struct list_element *current = &list_head;
-    if(new_element->next == current->next) {//if adding to the beginning
-        current->next = new_element;
-    }
-    while(current->next->next != new_element->next) { //while current is not pointing to the thing before the thing to  which new_element is pointing 
-        current->next = current->next->next;//make current point to the next item in the list
-        if(current->next == NULL) {//if current makes it to the end, stop
-            return 0;
+    struct list_element current = {list_head->next};
+    if(new_element->next == NULL) {//if adding an element that doesn't point to anything
+        while(current.next->next != current.next) {//go until it reaches the end
+            current.next = current.next->next;
+            if(current.next == current.next->next) { //if the list only has a head right now, just add it
+                break;
+            }
         }
+        current.next = new_element;
+        return;
     }
-    current->next->next = new_element;//make the thing to which current is pointing point to new_element
+    if(new_element->next == current.next) {//if adding to the beginning
+        current.next = new_element;
+        return;
+    }
+    while(current.next->next != new_element->next) { //while current is not pointing to the thing before the thing to  which new_element is pointing 
+        current.next = current.next->next;//make current point to the next item in the list
+    }
+    current.next->next = new_element;//make the thing to which current is pointing point to new_element
 }
 
 //removes an element from a linked list
 void list_remove(struct list_element *element) {
-    
+    element->data = element->next->data;
+    element->next = element->next->next;
 }
 
 unsigned long get_timer_count() {
@@ -55,14 +65,17 @@ void clear_bss() { // set all of bss to 0
 
 void kernel_main() {
 
-    struct list_element c = {NULL, 0}; // next ptr is NULL, end of list
-    struct list_element b = {&c, 0}; // next ptr points to c
-    struct list_element a = {&b, 0}; // next ptr points to a
+    struct list_element c = {NULL, 3}; // next ptr is NULL, end of list
+    struct list_element b = {&c, 2}; // next ptr points to c
+    struct list_element a = {&b, 1}; // next ptr points to a
     struct list_element *head = {&a};
-    list_add(head, a);
-    list_add(head, b);
-    list_add(head, c);
-
+    list_add(head, &c);
+    list_add(head, &b);
+    list_add(head, &a);
+    list_remove(&a);
+    list_remove(&b);
+    list_remove(&c);
+    
 
     clear_bss();
 
