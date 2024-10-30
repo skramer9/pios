966912b118848358b76dfec736e6cf87846c165a
Sam Kramer <skramer5@luc.edu>
2024-10-29 20:23:32 -0500

    Added something

--- src/string.c
+++ src/string.c
@@ -1,7 +1,7 @@
 // returns 0 if the strings don't match and 1 if they do
-int compareFilenames(char* x, char* y) {
-    while(*x != ' ' && *y != ' ') {
-        if (*x != *y) { //if the characters aren't the same, end
+int compareFilenames(char* x, char* y) {	
+while (*x != '\0' && *y != '\0' && *x != ' ' && *y != ' ') {
+	if (*x != *y) { //if the characters aren't the same, end
 	    return 0;
 	}
 	if (*x == *y) { //if they are, move on to the next chars
@@ -9,5 +9,5 @@ int compareFilenames(char* x, char* y) {
 	    y++;
         }
     }
-    return 1; //if the program makes it here, the strings matched
+    return (*x == *y || (*x == ' ' && *y == ' ') || (*x == '\0' && *y == '\0'));
 }
